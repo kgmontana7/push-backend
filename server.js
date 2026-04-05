0a00ef6bfd20be2777b63a51607b7a9af9463856
Kaj Montana <kvdno@users.noreply.github.com>
2026-04-05 18:51:36 +0200

    fix: firebase env support for render

--- server.js
+++ server.js
@@ -26,9 +26,17 @@ const saveDevices = () => {
 };
 
 // 🔥 FIREBASE INIT
-const serviceAccount = JSON.parse(
-  fs.readFileSync('./firebase-key.json', 'utf-8')
-);
+let serviceAccount;
+
+if (process.env.FIREBASE_KEY) {
+  // 🔥 Render / productie
+  serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+} else {
+  // 🔥 lokaal
+  serviceAccount = JSON.parse(
+    fs.readFileSync('./firebase-key.json', 'utf-8')
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
